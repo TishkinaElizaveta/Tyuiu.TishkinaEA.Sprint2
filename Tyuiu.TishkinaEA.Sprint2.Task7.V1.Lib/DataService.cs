c8161409599dfe37e22c0b47a3f1220e713e3532
admin <admin@DESKTOP-LHFSSMF>
2024-12-14 01:23:46 +0500

    zzzzzzz

---
+++ Tyuiu.TishkinaEA.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TishkinaEA.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            double distanceFromCenter = Math.Sqrt(x * x + y * y);
+            if (distanceFromCenter > 1)
+            {
+                return false;
+            }
+
+            if (y >= x && y >= -x && x >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint2.Task7.V1.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
