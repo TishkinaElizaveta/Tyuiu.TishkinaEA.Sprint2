5f8ce3969d6e7cd1e31bfe1875a967f0a1b1c868
admin <admin@DESKTOP-LHFSSMF>
2024-12-14 01:17:33 +0500

    a

---
+++ Tyuiu.TishkinaEA.Sprint2.Task6.V15.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TishkinaEA.Sprint2.Task6.V15.Lib
+{
+    public class DataService : ISprint2Task6V15
+    {
+        public string FindDayName(int k)
+        {
+            if (k < 1 || k > 365)
+            {
+                throw new ArgumentException("Некорректный номер дня. Допустимые значения: 1-365.");
+            }
+
+            // Определяем день недели с использованием сокращенной формы switch
+            return (k % 7) switch
+            {
+                1 => "Понедельник",
+                2 => "Вторник",
+                3 => "Среда",
+                4 => "Четверг",
+                5 => "Пятница",
+                6 => "Суббота",
+                0 => "Воскресенье",
+                _ => throw new InvalidOperationException("Неверный расчет дня недели.")
+            };
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint2.Task6.V15.Test/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint2.Task6.V15.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
