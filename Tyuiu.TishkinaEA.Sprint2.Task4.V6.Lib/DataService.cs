2ada01b9403945e7e0375f7b98e099411e3fb6ea
admin <admin@DESKTOP-LHFSSMF>
2024-12-14 01:07:59 +0500

    a

---
+++ Tyuiu.TishkinaEA.Sprint2.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TishkinaEA.Sprint2.Task4.V6.Lib
+{
+    public class DataService : ISprint2Task4V6
+    {
+        public double Calculate(double x, double y)
+        {
+            // Расчёт z с использованием тернарного оператора
+            double z = (x + 20 > y * 2)
+                ? x * Math.Pow((y + 1) / (x + 2), x)
+                : Math.Pow(y, 2) + 2 * x + (6 / x);
+
+            // Возвращаем результат
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint2.Task4.V6.Test/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint2.Task4.V6.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
